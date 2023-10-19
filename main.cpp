aa1ee4d38c65c9a02c7e769b4b4d56f34e5165e1
phzhu <phzhu99@gmail.com>
2023-10-19 15:38:08 -0400

    update cmakelists && add gflags

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
+#include <gflags/gflags.h>
+
 #include "src/calculate.h"
 #include "src/csv/csv_reader.h"
 
-int main() {
+DEFINE_int32(particle_num, 100, "number of particles to calculate");
 
-  int num = 100;
+int main(int argc, char *argv[]) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  printf("particle num:%d\n", FLAGS_particle_num);
 
-  auto particles = read_csv(num);
+  auto particles = read_csv(FLAGS_particle_num);
   calculate_closest(particles);
   auto forces = calculate_force(particles);
 
@@ -13,16 +17,6 @@ int main() {
     printf("index:%d, particle:%s, force:%e\n", i,
            particles[i].to_string().c_str(), forces[i]);
   }
-  // std::vector<Particle> ps;
-  // ps.push_back(Particle(3, 4, "p"));
-  // ps.push_back(Particle(3, 2, "p"));
-  // ps[0].closest = &ps[1];
-  // ps[1].closest = &ps[0];
-  // auto forces = calculate_force(ps);
-
-  // for (const auto &f : forces) {
-  //   printf("%e\n", f);
-  // }
 
   return 0;
 }
