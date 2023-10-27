9666d494244069ec9e6fe5655417950517378e85
Zhu  Penghui <zhupeng8@ug61.eecg.toronto.edu>
2023-10-27 13:29:12 -0400

    fix thread num

--- compare_modes.sh
+++ compare_modes.sh
@@ -3,6 +3,6 @@
 echo mode 1
 ./force_calculator --mode=1 --particle_num=100000000
 echo mode 2
-./force_calculator --mode=2 --particle_num=100000000 --thread_num=32
+./force_calculator --mode=2 --particle_num=100000000 --thread_num=16
 echo mode 3
-mpirun -np 5 ./force_calculator --mode=3 --particle_num=100000000 --thread_num=8 --chunk_size=10000
+mpirun -np 5 ./force_calculator --mode=3 --particle_num=100000000 --thread_num=4 --chunk_size=10000
--- test_mode3.sh
+++ test_mode3.sh
@@ -2,5 +2,5 @@
 
 for i in 10000 100000 1000000 10000000 100000000 
 do
-    mpirun -np 5 ./force_calculator --mode=3 --particle_num=$i --thread_num=8 --chunk_size=10000
+    mpirun -np 5 ./force_calculator --mode=3 --particle_num=$i --thread_num=4 --chunk_size=10000
 done
